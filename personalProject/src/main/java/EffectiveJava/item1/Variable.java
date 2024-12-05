404f79c0f55ff0ae65a9d9e11e41f9d0581cc8d4
youngwoo <dudaks0805@gmail.com>
2024-12-05 09:40:47 +0900

    first file(6)

---
+++ personalProject/src/main/java/EffectiveJava/item1/Variable.java
@@ -0,0 +1,11 @@
+package EffectiveJava.item1;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        // 변수 초기화를 하지 않으면 연산이 불가능 하다.
+        int value;
+        int result = value + 30;
+    }
+
+}
