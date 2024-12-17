ed974e6943ef508f9ee734537d5a239ea3496678
youngwoo <dudaks0805@gmail.com>
2024-12-17 09:32:20 +0900

    ~nested, anonymous

--- personalProject/src/main/java/EffectiveJava/anonymous/Chatting.java
+++ personalProject/src/main/java/EffectiveJava/anonymous/Chatting.java
@@ -14,7 +14,7 @@ public class Chatting {
             @Override
             public void start(){
                 while (true){
-                    String inputData = "안녕하세요";
+//                    String inputData = "안녕하세요";
 //                    String message = "[" + nickName +
                 }
             }
